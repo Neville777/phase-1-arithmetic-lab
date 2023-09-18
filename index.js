f675e9d6d56c8203fe8cd455443da2593c914ab0
neville777 <neville.james@student.moringaschool.com>
2023-09-19 01:40:35 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+// Define the values of num1 and num2
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+// Create the multiply variable with the equation
+const multiply = num1 * num2;
+
+// Define a fuction to generate a random integar > 0
+
+function getRandom() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+const random = getRandom();
+
+//  Define the values of num3 and num4
+
+const num3 = 20;
+const num4 = 16;
+
+// Create the mod variable with the eqation.
+// modulo or % is used to find the remainder
+
+const mod = num3 % num4
+
+// Define the numbers in the set
+const num9 = 10;
+const num8 = 15;
+const num7 = 20;
+const num6 = 5;
+
+// Calculate the maximum value using Math.max()
+const max = Math.max(num6, num7, num8, num9);
+
